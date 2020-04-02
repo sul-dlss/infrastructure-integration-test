a70ccccc9f7dc6e4934a3301621898e66335080b
Jeremy Nelson <jermnelson@gmail.com>
2020-04-02 16:34:22 -0600

    Dynamically changing spreadsheet

--- spec/features/bulk_update_metadata_spec.rb
+++ spec/features/bulk_update_metadata_spec.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
+require 'rubyXL'
+require 'rubyXL/convenience_methods/cell'
 
 RSpec.describe 'Use Argo to upload metadata in a spreadsheet', type: :feature do
-  let(:start_url) { 'https://argo-stage.stanford.edu/view/druid:qc410yz8746' }
+  let(:argo_home) { 'https://argo-stage.stanford.edu/' }
+  let(:start_url) { "#{argo_home}view/druid:qc410yz8746" }
+  let(:druid1_url) { "#{argo_home}view/fh141gk9610" }
+  let(:druid2_url) { "#{argo_home}view/mw605wr9855" }
+  let(:spec_location) { 'spec/fixtures/filled_template.xlsx' }
+  let(:title1) { RandomWord.phrases.next }
+  let(:title2) { RandomWord.phrases.next }
   let(:note) { RandomWord.phrases.next }
 
+
   before do
+    filled_xlsx = RubyXL::Parser.parse(spec_location)
+    sheet_1 = filled_xlsx.worksheets[0]
+    sheet_1[2][3].change_contents title1
+    sheet_1[3][3].change_contents title2
+    filled_xlsx.write(spec_location)
     authenticate!(start_url: start_url, expected_text: 'integration-testing')
   end
 
@@ -30,5 +44,15 @@ RSpec.describe 'Use Argo to upload metadata in a spreadsheet', type: :feature do
         break if page.has_text?(note)
       end
     end
+
+    # Delete's job run
+    byebug
+
+    # Open druids and tests for titles
+
+  end
+
+  after do
+    system "git checkout #{spec_location}"
   end
 end
