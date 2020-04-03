155cc8dff90b726605e9fc0f0dba6c82b0bfaddf
Naomi Dushay <ndushay@stanford.edu>
2020-04-03 13:30:33 -0700

    create_apo_spec: start on register apo url

--- spec/features/create_apo_spec.rb
+++ spec/features/create_apo_spec.rb
@@ -2,31 +2,27 @@
 
 RSpec.describe 'Use Argo to create an administrative policy object', type: :feature do
   let(:apo_title) { RandomWord.phrases.next }
-  let(:start_url) { 'https://argo-stage.stanford.edu/' }
+  let(:start_url) { 'https://argo-stage.stanford.edu/apo/new' }
 
   before do
-    authenticate!(start_url: start_url, expected_text: 'Welcome to Argo!')
+    expected_txt = 'The following defaults will apply to all newly registered objects.'
+    authenticate!(start_url: start_url, expected_text: expected_txt)
   end
 
   scenario do
-    # Activate the dropdown
-    click_link 'Register'
-    click_link 'Register APO'
-
-    expect(page).to have_content 'The following defaults will apply to all newly registered objects.'
-
     fill_in 'Title', with: apo_title
-    # TODO: More APO set-up steps
+    # TODO: More APO set-up steps / form fields exercised
     click_button 'Register APO'
 
-    # Make sure we're on an APO show view
+    # make sure we're on an APO show view
     expect(page).to have_content apo_title
+    # make sure APO is registered
     apo_druid = find('dd.blacklight-id').text
     expect(page).to have_content "APO #{apo_druid} created."
     object_type_element = find('dd.blacklight-objecttype_ssim')
     expect(object_type_element.text).to eq('adminPolicy')
 
-    # Wait for object to be accessioned
+    # wait for accessioningWF to finish
     Timeout.timeout(100) do
       loop do
         page.evaluate_script('window.location.reload()')
