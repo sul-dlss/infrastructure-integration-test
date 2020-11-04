720f4faef69592e5c4e432cf148aaba185cdad8b
Jeremy Nelson <jermnelson@gmail.com>
2020-11-04 09:06:27 -0700

    Continues reloading status page after async deposit of work

--- spec/features/create_object_h2_spec.rb
+++ spec/features/create_object_h2_spec.rb
@@ -59,6 +59,9 @@ RSpec.describe 'Use H2 to create an object', type: :feature do
     # Checks if title is on resulting display
     expect(page).to have_content("title = #{item_title}")
 
+    # Keep refreshing page until druid is available
+    reload_page_until_timeout!(text: 'druid = druid:')
+
     # Opens Argo and searches on title
     visit Settings.argo_url
 
