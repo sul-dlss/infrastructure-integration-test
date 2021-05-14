310f9f432d84457f0ae2fa149b2ee6d484749bac
Justin Coyne <jcoyne@justincoyne.com>
2021-05-14 14:52:08 -0500

    Test for text before reindexing
    There's no sense in running the reindex if the text already exists on the page

--- spec/support/page_helpers.rb
+++ spec/support/page_helpers.rb
@@ -4,16 +4,16 @@ module PageHelpers
   def reload_page_until_timeout!(text:, as_link: false, with_reindex: false)
     Timeout.timeout(Settings.timeouts.workflow) do
       loop do
-        if with_reindex
-          click_link 'Reindex'
-        else
-          page.driver.browser.navigate.refresh
-        end
-
         if as_link
           break if page.has_link?(text, wait: 1)
         else
           break if page.has_text?(text, wait: 1)
+        end
+
+        if with_reindex
+          click_link 'Reindex'
+        else
+          page.driver.browser.navigate.refresh
         end
       end
     end
