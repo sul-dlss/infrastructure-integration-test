a674a5c778c78458f14f057f954d95f8f74a91ba
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-11-01 13:46:52 -0700

    Alert the user if an unsupported environment is requested

--- spec/support/config.rb
+++ spec/support/config.rb
@@ -10,3 +10,8 @@ env = ENV.fetch('SDR_ENV', 'staging')
 Config.load_and_set_settings(
   Config.setting_files(config_root, env)
 )
+
+# NOTE: Added to flag situations where the wrong env is typed, e.g., `stage`
+unless Settings.supported_envs.include?(env)
+  raise "#{env} is not a supported environment: #{Settings.supported_envs.join(', ')}"
+end
