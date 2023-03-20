c67482753342a21918a1d93b84ac4ed118dd16cf
Peter Mangiafico <peter@mangiafico.org>
2023-03-20 11:05:19 -0700

    reset the shelve workflow step if it fails (#566)
    Co-authored-by: Michael J. Giarlo <mjgiarlo@stanford.edu>
    Fixes https://github.com/sul-dlss/infrastructure-integration-test/issues/556

--- spec/support/page_helpers.rb
+++ spec/support/page_helpers.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module PageHelpers
-  def reload_page_until_timeout!(text: '')
+  def reload_page_until_timeout!(text: '', check_wf_error: true)
     Timeout.timeout(Settings.timeouts.workflow) do
       loop do
         break if block_given? ? yield : page.has_text?(text, wait: 1)
 
         # Check for workflow errors and bail out early. There is no recovering
         # from a workflow error. This selector is found on the Argo item page.
-        expect(page).not_to have_css('.alert-danger', wait: 0)
+        expect(page).not_to have_css('.alert-danger', wait: 0) if check_wf_error
 
         page.refresh
       end
