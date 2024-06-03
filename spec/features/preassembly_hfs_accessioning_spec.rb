e4a965e6864efac0483b705fec3f004b80ff9335
Peter Mangiafico <peter@mangiafico.org>
2024-06-03 11:39:37 -0700

    OCR Image/Document Spec (#731)
    * ocr-image-spec
    
    * add a document ocr spec
    
    * remove manually finishing of ocr workflow
    
    * fix tests to deal with accessibility change
    
    * comment about how to unstick integration test filling in field too fast

--- spec/features/preassembly_hfs_accessioning_spec.rb
+++ spec/features/preassembly_hfs_accessioning_spec.rb
@@ -63,6 +63,8 @@ RSpec.describe 'Create and re-accession object with hierarchical files via Pre-a
     visit Settings.preassembly.url
     expect(page).to have_css('h1', text: 'Complete the form below')
 
+    # sleep 1 # if you notice the project name not filling in completely, try this to
+    #           give the page a moment to load so we fill in the full text field
     fill_in 'Project name', with: preassembly_project_name
     select 'Pre Assembly Run', from: 'Job type'
     select 'File', from: 'Content structure'
