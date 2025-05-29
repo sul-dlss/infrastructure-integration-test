b6c1381e514ed0d2caad9f6fc27ad5edcb7353c3
Johnathan Martin <john.martin@stanford.edu>
2025-05-28 21:03:49 -0700

    touchup cloud replication confirmation in wake of migration to GCP
    * centralize the code that confirms successful cloud replication of archive zips
    * remove expectation for replication to IBM
    * add expectation for replication to GCP
    
    closes https://github.com/sul-dlss/infrastructure-integration-test/issues/854
    closes https://github.com/sul-dlss/infrastructure-integration-test/issues/844

--- spec/features/preassembly_speech_to_text_media_spec.rb
+++ spec/features/preassembly_speech_to_text_media_spec.rb
@@ -167,45 +167,8 @@ RSpec.describe 'Create a media object via Pre-assembly and ask for it be speechT
     page.has_text?('filetype', count: 11)
     page.has_text?('file_modification', count: 11)
 
-    # The below confirms that preservation replication is working: we only replicate a
-    # Moab version once it's been written successfully to on prem storage roots, and
-    # we only log an event to dor-services-app after a version has successfully replicated
-    # to a cloud endpoint.  So, confirming that both versions of our test object have
-    # replication events logged for all three cloud endpoints is a good basic test of the
-    # entire preservation flow.
-    prefixed_druid = "druid:#{bare_druid}"
-    druid_tree_str = DruidTools::Druid.new(prefixed_druid).tree.join('/')
-
-    latest_s3_key = "#{druid_tree_str}.v0002.zip"
-    reload_page_until_timeout! do
-      click_link_or_button 'Events' # expand the Events section
-
-      # this is a hack that forces the event section to scroll into view; the section
-      # is lazily loaded, and won't actually be requested otherwise, even if the button
-      # is clicked to expand the event section.
-      page.execute_script 'window.scrollBy(0,100);'
-
-      # events are loaded lazily, give the network a few moments
-      page.has_text?(latest_s3_key, wait: 3)
-    end
-
-    # the event log should eventually contain an event for replication of each version that
-    # this test created to every endpoint we archive to
-    poll_for_matching_events!(prefixed_druid) do |events|
-      (1..2).all? do |cur_version|
-        cur_s3_key = "#{druid_tree_str}.v000#{cur_version}.zip"
-
-        %w[aws_s3_west_2 ibm_us_south aws_s3_east_1].all? do |endpoint_name|
-          events.any? do |event|
-            event[:event_type] == 'druid_version_replicated' &&
-              event[:data]['parts_info'] &&
-              event[:data]['parts_info'].size == 1 && # we only expect one part for this small object
-              event[:data]['parts_info'].first['s3_key'] == cur_s3_key &&
-              event[:data]['endpoint_name'] == endpoint_name
-          end
-        end
-      end
-    end
+    visit_argo_and_confirm_event_display!(druid:, version: 2)
+    confirm_archive_zip_replication_events!(druid:, from_version: 1, to_version: 2)
 
     # This section confirms the object has been published to PURL and has a
     # valid IIIF manifest
