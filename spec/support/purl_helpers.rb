443affe0fd5ada389dcadf7d16c40c950c26f090
Peter Mangiafico <peter@mangiafico.org>
2026-03-03 10:40:06 -0800

    this delay helps get past an argo timeout

--- spec/support/purl_helpers.rb
+++ spec/support/purl_helpers.rb
@@ -3,8 +3,7 @@
 # This module allows us to check the purl page for a given string
 module PurlHelpers
   def expect_text_on_purl_page(druid:, text:, within_frame: false)
-    bare_druid = druid.delete_prefix('druid:')
-    visit "#{Settings.purl_url}/#{bare_druid}"
+    visit "#{Settings.purl_url}/#{bare_druid(druid)}"
     sleep 1
     if within_frame
       reload_page_until_timeout! do
@@ -16,15 +15,13 @@ module PurlHelpers
   end
 
   def do_not_expect_text_on_purl_page(druid:, text:) # rubocop:disable Naming/PredicateMethod
-    bare_druid = druid.delete_prefix('druid:')
-    visit "#{Settings.purl_url}/#{bare_druid}"
+    visit "#{Settings.purl_url}/#{bare_druid(druid)}"
     sleep 1
     page.has_no_text?(text)
   end
 
   def expect_link_on_purl_page(druid:, text:, href:)
-    bare_druid = druid.delete_prefix('druid:')
-    visit "#{Settings.purl_url}/#{bare_druid}"
+    visit "#{Settings.purl_url}/#{bare_druid(druid)}"
     sleep 1
     reload_page_until_timeout! { page.has_link?(text, href:, wait: 2) }
   end
@@ -33,6 +30,10 @@ module PurlHelpers
     cocina_json = JSON.parse(Faraday.get("#{Settings.purl_url}/#{druid.delete_prefix('druid:')}.json").body)
     check_filenames = cocina_json['structural']['contains'].map { |node| node['structural']['contains'].first['filename'] }
     expect(check_filenames).to eq filenames
+  end
+
+  def bare_druid(druid)
+    druid.delete_prefix('druid:')
   end
 end
 
