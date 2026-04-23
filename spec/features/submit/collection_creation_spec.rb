402fbdf0fa074a45b003f2c3db0e36cc21975cb4
Aaron Collier <aaron.collier@stanford.edu>
2026-04-23 08:37:43 -0700

    Add collection creation and review

---
+++ spec/features/submit/collection_creation_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Integration: Argo, DSA
+RSpec.describe 'Use Argo to create a collection' do
+  let(:collection_title) { random_phrase }
+  let(:collection_abstract) { 'Created by https://github.com/sul-dlss/infrastructure-integration-test' }
+  let(:start_url) { "#{Settings.argo_url}/view/#{Settings.default_apo}" }
+
+  before do
+    authenticate!(start_url:,
+                  expected_text: 'integration-testing')
+  end
+
+  scenario do
+    click_link_or_button 'Create Collection'
+    fill_in 'Collection Title', with: collection_title
+    fill_in 'Collection Abstract', with: collection_abstract
+    click_link_or_button 'Register Collection'
+
+    expect(page).to have_text 'Created collection'
+
+    collection_druid = find('.alert-info').text.split[2]
+    puts " *** collection creation druid: #{collection_druid} ***" # useful for debugging
+    save_test_data(spec_name: 'collection_creation', data: { 'druid' => collection_druid, 'title' => collection_title })
+  end
+end
