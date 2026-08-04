ed38bc8e038ca46de939e709205c6109484a8665
Aaron Collier <aaron.collier@stanford.edu>
2026-08-04 14:31:39 -0700

    fix: add XPath string escaping in TableHelpers [t16]

--- spec/support/table_helpers.rb
+++ spec/support/table_helpers.rb
@@ -3,7 +3,21 @@
 # These helpers are particularly helpful given the design of Argo circa 2022
 module TableHelpers
   def find_table_cell_following(header_text:, xpath_suffix: '')
-    find(:xpath, "//tr/th[text()='#{header_text}']/following-sibling::td#{xpath_suffix}")
+    find(:xpath, "//tr/th[text()=#{xpath_string(header_text)}]/following-sibling::td#{xpath_suffix}")
+  end
+
+  private
+
+  # Properly escape a string for use in XPath expressions.
+  # Handles strings containing single quotes, double quotes, or both.
+  def xpath_string(str)
+    if !str.include?("'")
+      "'#{str}'"
+    elsif !str.include?('"')
+      "\"#{str}\""
+    else
+      "concat('#{str.split("'").join("',\"'\",'")}')"
+    end
   end
 end
 
