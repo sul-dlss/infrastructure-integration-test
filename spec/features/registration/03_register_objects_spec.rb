f8f8d045cc847677b9bf238a9b028093e8be6f75
Johnathan Martin <john.martin@stanford.edu>
2026-09-23 12:24:59 -0700

    typo fix

--- spec/features/registration/03_register_objects_spec.rb
+++ spec/features/registration/03_register_objects_spec.rb
@@ -3,12 +3,12 @@
 # Registers all of the expected objects for follow up tests
 # Utilizes the register_objects shared example
 RSpec.describe 'Register objects in Argo', :sample_accession, type: :registration do
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'access_indexing' }
   end
 
   context 'when registering a Goobi object', if: $sdr_env == 'stage' do
-    it_behaves_like 'an SDR object registion' do
+    it_behaves_like 'an SDR object registration' do
       let(:spec_name) { 'goobi_accessioning' }
       let(:apo) { 'Goobi Testing APO' }
       let(:collection) { 'integration-testing' }
@@ -19,7 +19,7 @@ RSpec.describe 'Register objects in Argo', :sample_accession, type: :registratio
     end
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'item_creation_no_files_or_collection' }
     let(:project) { 'Awesome Project' }
     let(:tags) { 'Some : UniqueTagValue' }
@@ -27,7 +27,7 @@ RSpec.describe 'Register objects in Argo', :sample_accession, type: :registratio
     let(:collection) { 'None' }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'item_creation_with_folio_hrid' }
     let(:project) { 'Awesome Folio Project' }
     let(:tags) { 'Some : UniqueTagValue' }
@@ -35,7 +35,7 @@ RSpec.describe 'Register objects in Argo', :sample_accession, type: :registratio
     let(:folio_hrid) { Settings.test_folio_instance_hrid }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'preassembly_gis_raster_accessioning' }
     let(:project_name) { 'Integration Test - GIS via preassembly' }
     let(:collection) { 'Integration Test Collection - GIS' }
@@ -43,7 +43,7 @@ RSpec.describe 'Register objects in Argo', :sample_accession, type: :registratio
     let(:type) { 'geo' }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'preassembly_gis_vector_accessioning' }
     let(:project_name) { 'Integration Test - GIS via preassembly' }
     let(:collection) { 'Integration Test Collection - GIS' }
@@ -51,31 +51,31 @@ RSpec.describe 'Register objects in Argo', :sample_accession, type: :registratio
     let(:type) { 'geo' }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'preassembly_hfs_accessioning' }
     let(:project) { 'Integration Test - hierarchical files via Preassembly' }
     let(:type) { 'file' }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'preassembly_ocr_document' }
     let(:project) { 'Integration Test - Document OCR via Preassembly' }
     let(:type) { 'document' }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'preassembly_ocr_image' }
     let(:project) { 'Integration Test - Image OCR via Preassembly' }
     let(:type) { 'image' }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'preassembly_accessioning' }
     let(:project) { 'Integration Test - Accessioning via Preassembly' }
     let(:type) { 'image' }
   end
 
-  it_behaves_like 'an SDR object registion' do
+  it_behaves_like 'an SDR object registration' do
     let(:spec_name) { 'preassembly_speech_to_text' }
     let(:project) { 'Integration Test - Media Speech To Text via Preassembly' }
     let(:type) { 'media' }
@@ -83,7 +83,7 @@ RSpec.describe 'Register objects in Argo', :sample_accession, type: :registratio
 
   context 'when registering virtual object constituents' do
     Settings.number_of_constituents.times do |i|
-      it_behaves_like 'an SDR object registion' do
+      it_behaves_like 'an SDR object registration' do
         let(:spec_name) { "virtual_object_creation_#{i}" }
         let(:project) { 'Integration Test - Virtual object via Preassembly' }
         let(:type) { 'image' }
